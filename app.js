9e1be0c39de41af3f1bca05583445d9a1e36eb71
Osama Nasr <o.nasr@breadfast.com>
2021-02-28 19:50:38 +0200

    add note approach

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
-const chalk = require('chalk');
-console.log(chalk.green('hello'))
-console.log(chalk.blue.bold('hello'))
-console.log(chalk.inverse.bold.italic('hello'))
+const yargs = require('yargs')
+const notes = require('./notes')
+
+yargs.command('read', "read note!", {}, () => console.log('read specific file'))
+yargs.command('list', "list all notes!", {}, () => console.log('list all notes'))
+yargs.command( 'add', "add new note", {
+  'body' : {
+    alias : 'b',
+    describe : "the body of the note",
+    type : "string",
+    demandOption : true
+  },'title' : {
+    alias : 't',
+    describe : "the title of the note",
+    type : "string",
+    demandOption : true
+  }
+  }, function(argv) {
+  notes.addNote(argv.title, argv.body)
+  }
+)
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,5 +1,40 @@
 const fs = require('fs')
+
 function getNotes(){
  return  fs.readFileSync('notes.txt','utf8')
 }
-module.exports = getNotes;
+
+function addNote(title, body){
+ const notes = loadNotes();
+ console.log(notes)
+ const duplicatedNotes = notes.filter((note) => note.title === title)
+ console.log(duplicatedNotes.length)
+ if (duplicatedNotes.length === 0 ){
+ notes.push({
+  title : title,
+  body : body
+ })
+  saveNotes(notes)
+  console.log('added successfully!');
+ }else {
+  console.log("this title already taken")
+ }
+
+}
+
+function loadNotes(){
+ try{
+  const notes = fs.readFileSync('notes.json', 'utf8')
+  return JSON.parse(notes)
+ }catch (e){
+  return  [];
+ }
+}
+
+function saveNotes(notes){
+ const saveNotes = JSON.stringify(notes)
+ fs.writeFileSync('notes.json', saveNotes);
+}
+module.exports = {
+ getNotes, addNote
+};
